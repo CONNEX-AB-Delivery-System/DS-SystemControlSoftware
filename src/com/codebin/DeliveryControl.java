9f679c213fd3fa09bc8c1a0c17250faf23ed4324
aivars.sablis <aivars.sablis@gmail.com>
2018-04-02 15:26:48 +0200

    connection control for all trucks

--- src/com/codebin/DeliveryControl.java
+++ src/com/codebin/DeliveryControl.java
@@ -10,32 +10,51 @@ import java.net.UnknownHostException;
 public class DeliveryControl {
 
     private JPanel panelMain;
-    private JFormattedTextField formattedTextField1;
+    private JFormattedTextField runCommandDTInput;
     private JFormattedTextField connectionStatusCSLT;
     private JFormattedTextField ipAddressCSLTInput;
     private JFormattedTextField formattedTextField4;
     private JFormattedTextField connectionStatusDT;
     private JFormattedTextField ipAddressDTInput;
-    private JTextArea textArea1;
+    private JTextArea outputFromDT;
     private JTextArea textArea2;
     private JButton connectDTButton;
     private JButton connectCSLTButton;
     private JLabel deliveryTruck;
     private JLabel CSLTruck;
     private JLabel ipAddressDTLabel;
-    private JTextField ipAddressFTInput;
+    private JFormattedTextField ipAddressFTInput;
     private JButton connectFTButton;
-    private JTextField connectionStatusFT;
     private JTextField textField3;
     private JTextArea textArea3;
     private JLabel ipAddressCSLTLabel;
     private JLabel ipAddressFTLabel;
     private JLabel forkliftTruck;
+    private JFormattedTextField connectionStatusFT;
+    private JLabel connectionStatusDTLabel;
+    private JButton disconnectDTButton;
+    private JLabel runCommandDTLabel;
+    private JButton runCommandDTButton;
 
     private BufferedWriter pw;
     private BufferedReader pr;
 
     public DeliveryControl() {
+        //set default values
+        ipAddressDTInput.setValue("172.16.42.25");
+        ipAddressCSLTInput.setValue("172.16.42.26");
+        ipAddressFTInput.setValue("172.16.42.24");
+
+
+
+
+       /* try {
+            pr.readLine();
+            outputFromDT.setText(pr.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }*/
+
         connectDTButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -67,14 +86,79 @@ public class DeliveryControl {
         connectCSLTButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                //TODO: Add connection to brick
+                String ipAddressCSLTValue = ipAddressCSLTInput.getText();
+                String connectionStatusCSLTValue = "";
+
+
+                try {
+                    //https://docs.oracle.com/javase/7/docs/api/java/net/Socket.html
+                    Socket socket = new Socket(ipAddressCSLTValue, 19231);
+//			        Socket socket = new Socket("127.0.0.1", 19231);//for mocking
+                    pw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+                    pr = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+                    connectionStatusCSLTValue = pr.readLine();
+                } catch (UnknownHostException exception) {
+                    // TODO Auto-generated catch block
+                    exception.printStackTrace();
+                } catch (IOException exception) {
+                    // TODO Auto-generated catch block
+                    exception.printStackTrace();
+                }
+
+
+                connectionStatusCSLT.setValue("connected to:" + connectionStatusCSLTValue);
             }
         });
 
         connectFTButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                //TODO: Add connection to brick
+                String ipAddressFTValue = ipAddressFTInput.getText();
+                String connectionStatusFTValue = "";
+
+
+                try {
+                    //https://docs.oracle.com/javase/7/docs/api/java/net/Socket.html
+                    Socket socket = new Socket(ipAddressFTValue, 19231);
+//			        Socket socket = new Socket("127.0.0.1", 19231);//for mocking
+                    pw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+                    pr = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+                    connectionStatusFTValue = pr.readLine();
+                } catch (UnknownHostException exception) {
+                    // TODO Auto-generated catch block
+                    exception.printStackTrace();
+                } catch (IOException exception) {
+                    // TODO Auto-generated catch block
+                    exception.printStackTrace();
+                }
+
+
+                connectionStatusFT.setValue("connected to:" + connectionStatusFTValue);
+            }
+        });
+
+        runCommandDTButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String runCommandDTValue = runCommandDTInput.getText();
+
+                switch (runCommandDTValue) {
+                    case "left":
+                        sendCommand("LEFT-PRESS");
+                        break;
+                    case "right":
+                        sendCommand("RIGHT-PRESS");
+                        break;
+                }
+            }
+        });
+        disconnectDTButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                sendCommand("STOP");
+                connectionStatusDT.setValue("disconnected");
             }
         });
     }
@@ -87,6 +171,7 @@ public class DeliveryControl {
         frame.pack();
         frame.setVisible(true);
 
+
     }
 
     private void sendCommand(String command) {
@@ -96,5 +181,6 @@ public class DeliveryControl {
             e1.printStackTrace();
         }
     }
+
 
 }
